4dd053fce73447362856a794cec27a32f3eb94fc
Sameer D'Costa <sameerdcosta@gmail.com>
2018-09-17 23:02:22 -0500

    Send in separate jobs

--- projects/family/scripts/create_gvcf.bash
+++ projects/family/scripts/create_gvcf.bash
@@ -1,28 +1,12 @@
 #!/bin/bash
 
-#MSUB -e logs/errlog.dat
-#MSUB -o logs/outlog.dat
-
-#MSUB -A b1042
-#MSUB -l naccesspolicy=singlenode
-#MSUB -l walltime=167:00:00 
-#MSUB -q genomicslong
-
-
-
 set -x
 
-# Set your working directory
-# Required for quest
-if [[ -v PBS_O_WORKDIR ]]; then cd $PBS_O_WORKDIR; fi
 
 source ./scripts/global_variables.bash
 source ./scripts/project_variables.bash
 
 echo ${OUTPUTDIR}
-
-##first need to load the correct version of java (v2.8)
-module load java
 
 
 # Create gvcf files for members of family 2
@@ -31,12 +15,35 @@ do
   input_var="G${i}_BAM_READY"
   output_var="G${i}_G_VCF"
 
-  ${JAVA} -jar ${GATK} \
+ JOB=`msub - <<EOJ
+
+#MSUB -A b1042
+#MSUB -l naccesspolicy=singlenode
+#MSUB -l walltime=167:00:00 
+#MSUB -q genomicslong
+
+#MSUB -e logs/errlog.dat
+#MSUB -o logs/outlog.dat
+
+# Set your working directory
+# Required for quest
+if [[ -v PBS_O_WORKDIR ]]; then cd $PBS_O_WORKDIR; fi
+
+##first need to load the correct version of java (v2.8)
+module load java
+
+${JAVA} -jar ${GATK} \
     -T HaplotypeCaller \
     -R "${GENOMEREF_37}" \
     --emitRefConfidence GVCF \
     -I ${!input_var} \
     -o ${!output_var}
+
+EOJ
+`
+
+echo "Sending in ${JOB}"
+
 
 done
 
