73f6eaed97fa1e7709542dbee163bafa511a8127
Sameer D'Costa <sameerdcosta@gmail.com>
2018-10-17 20:10:38 -0500

    Plots of shared ibd segments

--- projects/family/scripts/plot_ibd.R
+++ projects/family/scripts/plot_ibd.R
@@ -13,35 +13,44 @@ all(as.character(x$sample1) < as.character(x$sample2))
 colnames(x) <- c("sample1", "sample1idx", "sample2", "sample2idx", 
                  "chr", "startpos", "endpos", "lodscore", "lengthcm")
 
+
+family1 <- c("SS4009017", "SS4009018", "SS4009019", "SS4009020")
+family2 <- c("SS4009021", "SS4009023", "SS4009030")
+
+family_samples <- family2
+
 subx <- x %>% 
- filter(sample1 == "SS4009021") %>% 
- filter(sample2 == "SS4009023") %>% 
- filter(lodscore > 3) %>%
+ filter(sample1 %in% family_samples) %>% 
+ filter(sample2 %in% family_samples) %>% 
+ filter(lodscore > 1) %>%
  identity()
 
 #head(subx, 10)
 
-sample1 = as.character(head(subx$sample1, 1))
-sample2 = as.character(head(subx$sample2, 1))
 chr = as.character(head(subx$chr, 1))
 
 ggplot(subx, aes(xmin=sample1idx - 0.25,
                  xmax=sample1idx + 0.25,
                  ymin=startpos,
-                 ymax=endpos)
-      ) + theme(
-        panel.grid.minor = element_blank()
-      ) + scale_x_discrete(
-        name=paste(head(subx$sample1, 1), "Haplotype Index"),
-        limits=c("1", "2") 
-      ) + scale_y_continuous(
-        name="Chromosome Position",
-        labels=scales::unit_format(unit="Mb", scale=1e-6)
-      ) + geom_rect(
-        aes(fill=factor(sample2idx))
-      ) + ggtitle(
-        paste0(sample1, "-", sample2, " : Chr", chr)
-      ) + labs(
-        fill = paste(sample2, "\nHapIndex")
-      )
+                 ymax=endpos)) + 
+    theme( 
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(size=0.1, color="white")) +    scale_x_discrete( 
+        name="Lower Sample Haplotype Index",
+        limits=c("1", "2")) + 
+    scale_y_continuous( 
+        name="Chr Position",
+        labels=scales::unit_format(unit="Mb", scale=1e-6)) + 
+    geom_rect( aes(fill=factor(sample2idx))) + 
+    ggtitle( paste("Chromosome", chr)) + 
+    labs( fill = paste("Upper Sample\nHapIndex")) + 
+    facet_wrap( ~sample2 + sample1) + 
+    annotate("rect", xmin=c(1-0.25, 2-0.25), 
+                     xmax=c(1+0.25, 2+0.25),
+                     ymin=0, ymax=Inf, 
+                     fill="grey", alpha=0.5, 
+                     linetype=2, size=1)
 
+ggsave("~/chr1_family2.png")    
+#ggsave("~/chr1_family1.png")    
