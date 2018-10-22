b668c8057defe8ff9a1323e4f6deb0873f6b666f
Sameer D'Costa <sameerdcosta@gmail.com>
2018-10-22 09:02:28 -0500

    Using gatk3

--- projects/family/scripts/extract_family2_samples_from_pre_gen.bash
+++ projects/family/scripts/extract_family2_samples_from_pre_gen.bash
@@ -10,28 +10,17 @@ echo ${OUTPUTDIR}
 ##first need to load the correct version of java (v2.8)
 module load java
 
-# FIXME: Need to remove idx files before running combine variants
 
-#${JAVA} -jar ${GATK} \
-#   -T SelectVariants \
-#   -R "${GENOMEREF_37}" \
-#   -V "${PRE_GEN_VCF_SORTED}" \
-#   -o "${MERGED2_VCF}" \
-#   -sn "SS400${C1}" \
-#   -sn "SS400${P1}" \
-#   -sn "SS400${P2}" \
-#   -sn "SS400${C2}" 
-
-# Using GATK4 because of a bug in gatk3
-
-${GATK4} SelectVariants \
+${JAVA} -jar ${GATK} \
+   -T SelectVariants \
    -R "${GENOMEREF}" \
    -V "${PRE_GEN_VCF_SORTED}" \
-   -O "${MERGED2_VCF}" \
-   --remove-unused-alternates true \
+   -o "${MERGED2_VCF}" \
+   -env -ef \
    -sn "SS400${G1}" \
    -sn "SS400${G2}" \
    -sn "SS400${G3}" 
+
 
 
 
