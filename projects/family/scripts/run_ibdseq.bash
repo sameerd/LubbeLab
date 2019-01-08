9a49c6b9a1434451ddc4dfe69f59335d0539c17e
Sameer D'Costa <sameerdcosta@gmail.com>
2019-01-08 08:12:54 -0600

    More reorg

--- projects/family/scripts/run_ibdseq.bash
+++ projects/family/scripts/run_ibdseq.bash
@@ -7,13 +7,13 @@ source ./scripts/project_variables.bash
 
 mkdir -p "${BEAGLE_OUTPUT_DIR}"
 
-chr=1
+#chr=1
 
 chromosomes=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22)
  
 for chr in ${chromosomes[@]}; do
   ${JAVA} -jar ${IBDSEQ} \
-        gt=${PRE_GEN_VCF_SORTED_QC} \
+        gt=${ALL_VCF_SORTED_QC} \
         out=${BEAGLE_OUTPUT_DIR}/ibd_seq_${chr} \
         chrom=chr${chr} \
         ibdlod=2.0 
