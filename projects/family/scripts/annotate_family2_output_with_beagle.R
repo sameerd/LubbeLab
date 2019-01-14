674b0b1879f60fab0e3a4f7cb2194f8391f6eeb1
Sameer D'Costa <sameerdcosta@gmail.com>
2019-01-14 11:32:45 -0600

    Family 2 changes

--- projects/family/scripts/annotate_family2_output_with_beagle.R
+++ projects/family/scripts/annotate_family2_output_with_beagle.R
@@ -1,22 +1,20 @@
 library(tidyverse)
 
-# sample names
-family2.samplenames <- paste0("SS40090", c("21", "23", "30"))
-affected.family2.samplenames  <-  family2.samplenames[c(1,3)]
-
 # read in beagle IBD output
-family.shared.segments.script <- 'zcat data/working/beagle_output/ibd_out_*.ibd.gz'
+family.shared.segments.script <- 'cat data/working/beagle_output/ibd_seq_*.ibd'
 x <- read.table(pipe(family.shared.segments.script))
 colnames(x) <- c("sample1", "hapindex1", "sample2", "hapindex2", "chr", 
-                 "startpos", "endpos", "lod", "size")
+                 "startpos", "endpos", "lod")
 # save the output in a compatible db
 beagle.db <- x %>%  
-        mutate(chr = paste0("chr", chr)) %>%
+        #mutate(chr = paste0("chr", chr)) %>%
         identity
 
 # read in the hits file
-hits <- read.table("data/output/family2/generation_variants_dominant.txt", sep="\t",
+hits <- read.table("data/output/family2/dominant.txt", sep="\t",
                     header=TRUE)
+
+affected.family2.samplenames <-  select_vars(colnames(hits), starts_with("SS400"))
 
 GetBeagleLod <- function(chr, loc, sample1.name, sample2.name, beagle.db) {
   # Get the beagle lod score if we have a match
@@ -40,16 +38,15 @@ GetBeagleLod("chr10", 8000000,
              affected.family2.samplenames[2],
              beagle.db=beagle.db)
 
-
 beagle.annotated.hits <- hits %>%
   rowwise() %>%
-  mutate(beagle_lod_21_30=GetBeagleLod(Chr, Start, "SS4009021", "SS4009030", beagle.db)) %>%
-  mutate(beagle_lod_21_23=GetBeagleLod(Chr, Start, "SS4009021", "SS4009023", beagle.db)) %>%
-  mutate(beagle_lod_23_30=GetBeagleLod(Chr, Start, "SS4009023", "SS4009030", beagle.db)) %>%
+  mutate(beagle_lod_21_30=GetBeagleLod(CHROM, POS, "SS4009021", "SS4009030", beagle.db)) %>%
+  mutate(beagle_lod_21_23=GetBeagleLod(CHROM, POS, "SS4009021", "SS4009023", beagle.db)) %>%
+  mutate(beagle_lod_23_30=GetBeagleLod(CHROM, POS, "SS4009023", "SS4009030", beagle.db)) %>%
   identity
 
 write.table(beagle.annotated.hits %>% as.data.frame, 
-            file="data/output/family2/generation_variants_dominant_beagle.txt", 
+            file="data/output/family2/dominant_beagle.txt", 
             sep="\t", quote=FALSE, row.names=FALSE)
 
 
