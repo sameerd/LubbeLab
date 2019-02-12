77fc0f77c71d4a7a2b8e103692fb1412d75d9265
Sameer D'Costa <sameerdcosta@gmail.com>
2019-02-12 15:50:08 -0600

    ucla project

---
+++ projects/wgs_pipeline/README.md
@@ -0,0 +1,2 @@
+Misc pipeline projects go in this directory. Mostly they will use the scripts
+in the [cromwell directory](../../cromwell/)
---
+++ projects/wgs_pipeline/UCLA_Feb2019/alignment_pipeline_submit.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+cwd=`pwd`
+
+cromwell_dir="../../../cromwell"
+
+msub -A b1042 \
+  -e "${cwd}/logs/errlog.txt" \
+  -o "${cwd}/logs/outlog.txt" \
+  -d "${cwd}" \
+  -q genomics \
+  -l mem=4gb \
+  -l walltime=48:00:00 \
+  -l nodes=1:ppn=1 \
+  - <<EOJ
+
+#!/bin/bash
+
+module load java
+
+java -Dconfig.file=$cromwell_dir/cromwell_config.conf \
+     -jar /projects/b1049/genetics_programs/cromwell/cromwell-35.jar \
+     run -i alignment_pipeline.json \
+     $cromwell_dir/alignment_pipeline.wdl
+
+
+EOJ
+
