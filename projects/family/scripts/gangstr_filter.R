14e668e53b237d1c99b8e2cce3a0f588681c8313
Sameer D'Costa <sameerdcosta@gmail.com>
2019-03-27 20:21:30 -0500

    Separate out common functionality

---
+++ projects/family/scripts/gangstr_filter.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+# These are common functionality to filter gangSTR output
+
+# select whether we are looking at the gangSTR output
+is.filtered = ""
+# or the vcf filtered with dumpSTR
+is.filtered = ".filtered"
+
+# pull in all the processed vcf files
+get_filtered_vcf_file <- function(sample.name, 
+                                  file.extension=paste0(is.filtered, ".txt")) {
+  read.csv(paste0("data/working/gangstr/", sample.name, file.extension),
+                  sep="\t", stringsAsFactors=FALSE)
+} 
+
+get_new_column_names <- function(x, sample.extension=paste0(is.filtered, ".vcf")) {
+  # one of the columns is the sample vcf filename. e.g. SS4009013.vcf
+  # find out which one
+  idx <- grep("SS400", colnames(x))
+  if (length(idx) != 0) {
+    # shorten the sample name to something like SS13
+    sample.short.index <- gsub(paste0("SS40090([0-9]+)", sample.extension), "SS\\1", colnames(x)[idx])
+    
+    # Keep all columns below idx the same. Rename the idx column and the ones after
+    # to look like SS13_[original_column_name]
+    c(colnames(x)[1:(idx-1)], 
+      paste0(sample.short.index, "_NUM"), 
+      paste0(sample.short.index, "_", colnames(x[(idx+1):length(x)])))
+  } else { # we could not find SS400 so return the same column names
+    colnames(x)
+  }
+}
+
+get_filtered_vcf_file_with_cols <- function(sample.name, is.filtered) {
+  x <- get_filtered_vcf_file(sample.name, 
+                             file.extension=paste0(is.filtered, ".txt")) 
+  colnames(x) <- get_new_column_names(x, 
+                    sample.extension=paste0(is.filtered, ".vcf"))
+  x
+}
+
+
+# separate confidence interval columns
+# converts resulting values to integer
+separate_ci <- function(x, column) {
+  # column looks like SS16_CI
+  intermediate.columns =  paste(column, 1:2, sep="_")
+  x %>% separate(!! column, sep=",", intermediate.columns) %>%
+        separate(!! intermediate.columns[1], sep="-", 
+                 paste(intermediate.columns[1], c("L", "U"), sep="_"),
+                 convert=TRUE)  %>%
+        separate(!! intermediate.columns[2], sep="-", 
+                 paste(intermediate.columns[2], c("L", "U"), sep="_"),
+                 convert=TRUE)  %>%
+        identity()
+}
+
+# annotate a dataframe with annovar
+annotate_db <- function(x, working.gangstr.file) {
+  # db to send to annovar
+  # working.gangstr.file is where we store the file we sent to annovar 
+  x.annovar <- x %>% 
+            mutate(CHROM=paste0("chr", CHROM)) %>%
+            add_column(FAKEREF="0", .after="END") %>%
+            add_column(FAKEWT="0", .after="FAKEREF")
+  write_tsv(x.annovar, working.gangstr.file, col_names=FALSE)
+
+  # now call Annovar
+  system(paste("/projects/b1049/genetics_programs/annovar_2017/annovar/table_annovar.pl",
+        working.gangstr.file, 
+       "/projects/b1049/genetics_programs/annovar_2017/annovar/humandb/ \\
+       -buildver hg19 -otherinfo -protocol refGene,genomicSuperDups -operation g,r -nastring ." ))
+
+  # Pull in the annotated file and rename columns
+  multianno.output.file <- paste0(working.gangstr.file, ".hg19_multianno.txt")
+  ann <- read.csv(multianno.output.file, sep="\t", stringsAsFactors=FALSE, header=FALSE, skip=1)
+  # pull out the header column separately
+  ann.header <- strsplit(readLines(multianno.output.file, n=1), "\t")[[1]]
+  
+  # The first 5 columns of x.annovar belong to Annovar and it rearrages them as it sees fit
+  # From column 6 onwards we have otherinfo. 
+  # So we pick the annovar headers from the annovar output file
+  # and we pick the otherinfo headers from the input file
+  colnames(ann) <- c(ann.header[1:(length(ann.header)-1)], 
+                     colnames(x.annovar)[6:length(colnames(x.annovar))])
+  
+  # Remove the fake columns and return
+  ann %>% select(-Ref, -Alt) 
+}
+
+
--- projects/family/scripts/gangstr_filter_family3.R
+++ projects/family/scripts/gangstr_filter_family3.R
@@ -1,45 +1,15 @@
 library(tidyverse)
 
-# select whether we are looking at the gangSTR output
+# Common gangstr filtering 
+source("scripts/gangstr_filter.R")
+
+#is.filtered = ".filtered"
 is.filtered = ""
-# or the vcf filtered with dumpSTR
-is.filtered = ".filtered"
 
-# pull in all the processed vcf files
-get_filtered_vcf_file <- function(sample.name, 
-                                  file.extension=paste0(is.filtered, ".txt")) {
-  read.csv(paste0("data/working/gangstr/", sample.name, file.extension),
-                  sep="\t", stringsAsFactors=FALSE)
-} 
-x1 <- get_filtered_vcf_file("SS4009013")
-x2 <- get_filtered_vcf_file("SS4009014")
-x3 <- get_filtered_vcf_file("SS4009015")
-x4 <- get_filtered_vcf_file("SS4009016")
-
-
-get.new.column.names <- function(x, sample.extension=paste0(is.filtered, ".vcf")) {
-  # one of the columns is the sample vcf filename. e.g. SS4009013.vcf
-  # find out which one
-  idx <- grep("SS400", colnames(x))
-  if (length(idx) != 0) {
-    # shorten the sample name to something like SS13
-    sample.short.index <- gsub(paste0("SS40090([0-9]+)", sample.extension), "SS\\1", colnames(x)[idx])
-    
-    # Keep all columns below idx the same. Rename the idx column and the ones after
-    # to look like SS13_[original_column_name]
-    c(colnames(x)[1:(idx-1)], 
-      paste0(sample.short.index, "_NUM"), 
-      paste0(sample.short.index, "_", colnames(x[(idx+1):length(x)])))
-  } else { # we could not find SS400 so return the same column names
-    colnames(x)
-  }
-}
-
-colnames(x1) <- get.new.column.names(x1)
-colnames(x2) <- get.new.column.names(x2)
-colnames(x3) <- get.new.column.names(x3)
-colnames(x4) <- get.new.column.names(x4)
-
+x1 <- get_filtered_vcf_file_with_cols("SS4009013", is.filtered)
+x2 <- get_filtered_vcf_file_with_cols("SS4009014", is.filtered)
+x3 <- get_filtered_vcf_file_with_cols("SS4009015", is.filtered)
+x4 <- get_filtered_vcf_file_with_cols("SS4009016", is.filtered)
 
 # merge all the data frames together
 x <- x1 %>% inner_join(x2, by=c("CHROM", "POS", "END", "RU", "REF")) %>% 
@@ -50,8 +20,6 @@ colnames(x)
 # remove input dataframes now that we have joined everything
 #rm(x1, x2, x3, x4)
 
-
-
 # let's remove all rows where this family has the same genotype
 # these rows are not interesting. This removes 0/0 genotypes mostly
 # For now let us assume that 0/1 and 1/0 are different 
@@ -60,23 +28,6 @@ x %<>% filter(! ((SS13_GT == SS14_GT) & (SS14_GT == SS15_GT) &
        filter_at(vars(ends_with("_GT")), all_vars(. != "./.")) %>%
        filter_at(vars(ends_with("_GT")), all_vars(. != ".")) %>%
        identity()
-
-
-# separate confidence interval columns
-# converts resulting values to integer
-separate_ci <- function(x, column) {
-  # column looks like SS16_CI
-  intermediate.columns =  paste(column, 1:2, sep="_")
-  x %>% separate(!! column, sep=",", intermediate.columns) %>%
-        separate(!! intermediate.columns[1], sep="-", 
-                 paste(intermediate.columns[1], c("L", "U"), sep="_"),
-                 convert=TRUE)  %>%
-        separate(!! intermediate.columns[2], sep="-", 
-                 paste(intermediate.columns[2], c("L", "U"), sep="_"),
-                 convert=TRUE)  %>%
-        identity()
-}
-
 
 # separate all confidence interval columns
 x %<>% separate_ci("SS13_CI") %>%
@@ -102,39 +53,9 @@ x.inherit <- x %>%
   identity()
 
 working.gangstr.file <-  "data/working/family3_gangstr/annovar.tsv"
+ann <- annotate_db(x.inherit, working.gangstr.file)
 
-#db to send to annovar
-# In order to send this to annovar we add a FAKEREF and FAKEWT column
-# of zeros
-x.annovar <- x.inherit %>% 
-            mutate(CHROM=paste0("chr", CHROM)) %>%
-            add_column(FAKEREF="0", .after="END") %>%
-            add_column(FAKEWT="0", .after="FAKEREF")
-write_tsv(x.annovar, working.gangstr.file, col_names=FALSE)
-
-system(paste("/projects/b1049/genetics_programs/annovar_2017/annovar/table_annovar.pl",
-        working.gangstr.file, 
-       "/projects/b1049/genetics_programs/annovar_2017/annovar/humandb/ \\
-       -buildver hg19 -otherinfo -protocol refGene,genomicSuperDups -operation g,r -nastring ." ))
-
-# Pull in the annotated file and rename columns
-multianno.output.file <- paste0(working.gangstr.file, ".hg19_multianno.txt")
-ann <- read.csv(multianno.output.file, sep="\t", stringsAsFactors=FALSE, header=FALSE, skip=1)
-# pull out the header column separately
-ann.header <- strsplit(readLines(multianno.output.file, n=1), "\t")[[1]]
-
-# The first 5 columns of x.annovar belong to Annovar and it rearrages them as it sees fit
-# From column 6 onwards we have otherinfo. 
-# So we pick the annovar headers from the annovar output file
-# and we pick the otherinfo headers from the input file
-colnames(ann) <- c(ann.header[1:(length(ann.header)-1)], 
-                   colnames(x.annovar)[6:length(colnames(x.annovar))])
-
-
-# Remove the fake columns and write out to file
-# Also modify some scores and split intergenic genes into two columns
-ann %>% 
-  select(-Ref, -Alt)  %>%
+ann %>%
   mutate(SingleRefGene = Gene.refGene) %>%
   separate_rows(SingleRefGene, sep=";") %>%
   extract(genomicSuperDups, into=c("superDupsScore"), 
